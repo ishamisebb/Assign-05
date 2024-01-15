629171191c4347b57a17fabd2921b5feb1a0f2d1
Ishami Sebgoya <ishami.sebgoya@stu.ocsb.ca>
2024-01-15 02:04:50 +0000

    Added Assign-05.py

---
+++ Assign-05.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+# Created by: Ishami Sebgoya
+# Created on: December 6th 2023
+# This program Asks the user for the length, width and height of a rectangular prism. Then calculate and display its volume.
+
+ 
